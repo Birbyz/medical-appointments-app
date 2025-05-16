8175e3723d5a5406cfc910bad401e38db8c8d591
Birbyz <imbirbyz@gmail.com>
2025-05-17 02:34:37 +0300

    add navigation to the log in page

---
+++ app/src/main/java/com/example/medicalappointments/ui/login/LoginFragment.kt
@@ -0,0 +1,18 @@
+package com.example.medicalappointments.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.medicalappointments.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_login, container, false)
+
+}
