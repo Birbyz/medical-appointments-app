80454c15ca15c614bf3dd21bb005e8eb5987ef65
Birbyz <imbirbyz@gmail.com>
2025-05-17 03:04:19 +0300

    add register screen

--- app/src/main/java/com/example/medicalappointments/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/medicalappointments/ui/login/LoginFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.medicalappointments.R
 
 class LoginFragment : Fragment() {
@@ -15,4 +18,17 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_login, container, false)
 
+//    Primeste o referinta a view-ului ce trebuie sa il deseneze
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.tv_register).setOnClickListener{
+            goToRegister()
+        }
+    }
+
+    private fun goToRegister() {
+        val action = LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+        findNavController().navigate(action)
+    }
 }
