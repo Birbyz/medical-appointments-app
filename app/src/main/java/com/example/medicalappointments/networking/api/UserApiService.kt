3891682a08b239883de1a136d8e339753b8e6b32
Birbyz <imbirbyz@gmail.com>
2025-05-18 17:24:22 +0300

    add a method which returns users from a server

--- app/src/main/java/com/example/medicalappointments/networking/api/UserApiService.kt
+++ app/src/main/java/com/example/medicalappointments/networking/api/UserApiService.kt
@@ -3,8 +3,10 @@ package com.example.medicalappointments.networking.api
 import com.example.medicalappointments.networking.models.UserListAPIResponse
 import retrofit2.http.Query
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface UserApiService{
     @GET("/api/users")
+    @Headers("x-api-key: reqres-free-v1")
     suspend fun getUsers(@Query("page") page: Int): UserListAPIResponse
 }
--- app/src/main/java/com/example/medicalappointments/ui/users/UsersFragment.kt
+++ app/src/main/java/com/example/medicalappointments/ui/users/UsersFragment.kt
@@ -6,13 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.medicalappointments.R
 import com.example.medicalappointments.adapters.UsersAdapter
 import com.example.medicalappointments.models.UserModel
+import com.example.medicalappointments.networking.repository.AuthenticationRepository
+import com.example.medicalappointments.networking.repository.UserRepository
+import com.example.medicalappointments.utils.extensions.showToast
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okio.IOException
+import retrofit2.HttpException
 
 class UsersFragment: Fragment() {
+
+    private val users = mutableListOf<UserModel>()
+    private val adapter = UsersAdapter(users)
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,19 +35,30 @@ class UsersFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = UsersAdapter(
-            listOf(UserModel(
-                id = "0",
-                firstName = "First Name",
-                lastName = "Last Name",
-                email = "email",
-                avatar = "https://picsum.photos/200"
-            ))
-        )
-
         view.findViewById<RecyclerView>(R.id.rv_users).apply {
-            this.adapter = adapter
+            this.adapter = this@UsersFragment.adapter
             this.layoutManager = LinearLayoutManager(this.context)
+        }
+
+        getUsersFromServer()
+    }
+
+    private fun getUsersFromServer() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            try {
+                val result = withContext(Dispatchers.IO) {
+                    UserRepository.getUsers(1)
+                }
+                users.clear()
+                users.addAll(result.data)
+                adapter.notifyItemChanged(0, users.size)
+            } catch (e: IOException) {
+                ("Please check your internet connection").showToast(requireContext())
+            } catch (e: HttpException) {
+                ("Server error: ${e.code()}".showToast(requireContext()))
+            } catch (e: Exception) {
+                ("Unexpected error: ${e.localizedMessage}").showToast(requireContext())
+            }
         }
     }
 }
